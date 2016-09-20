32a0016f7ed333edad3b81e34db4a5bdb6e778ac
Sebastian <s.kaupper@gmx.at>
2016-09-20 13:36:08 +0200

    fix generator include

--- DataStructureGenerator.py
+++ DataStructureGenerator.py
@@ -33,7 +33,7 @@ print("Absolute output path: " + outputPath)
 
 
 # reference to StructConverter.h       
-converterHeaderFile = "include/StructConverter.h"
+converterHeaderFile = "StructConverter.h"
 
 # references to serialize templates
 toJSONTemplates = "templates/ToJSONTemplates.cpp"
